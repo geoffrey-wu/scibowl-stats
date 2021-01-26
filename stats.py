064fd89b72a502001ef50539ec85eaa1177a6541
Geoffrey Wu <geoffrey.feifei@gmail.com>
2021-01-25 21:39:32 -0600

    code refactoring

--- stats.py
+++ stats.py
@@ -1,4 +1,3 @@
-
 import csv
 import json
 import os
@@ -11,7 +10,7 @@ with open('key.json') as f:
     json_data = json.load(f)
 
 # different subject categories
-cats = [cat for cat in json_data['subjects'].keys()]
+cats = [cat for cat in json_data['categories'].keys()]
 
 # file directory that contains all of the scoresheets
 directory = json_data['directory']
@@ -23,16 +22,16 @@ codes = json_data['codes']
 
 # given a string, returns the question category
 def get_category(category):
-    category = category.lower().strip()
+    category = str(category).lower().strip()
     for cat in cats:
-        if category in json_data['subjects'][cat]:
+        if category in json_data['categories'][cat]:
             return cat
     return 'n/a'
 
 
 # given a cell value, returns the correct index for the code
 def get_code_index(cell):
-    cell = str(cell).upper().strip()
+    cell = str(cell).lower().strip()
     if cell in codes['interrupt_correct']:
         return 0
     elif cell in codes['correct']:
@@ -52,7 +51,7 @@ def get_question_row(game):
     return -1
 
 
-# read all of the players in the rosters file
+### read all of the players in the rosters file ###
 rosters = {}
 if json_data['rosters'] != '':
     for line in open(json_data['rosters'], 'r'):
@@ -67,38 +66,42 @@ teams = [team for team in set(rosters.values())]
 player_stats = {}
 
 # dictionary containing per-team bonus stats
-team_bonus_stats = {}
-for team in rosters.values():
-    if team not in team_bonus_stats:
-        team_bonus_stats[team] = {
-            'GP': 0
-        }
-        for cat in cats:
-            team_bonus_stats[team][cat] = [0, 0]
+bonus_stats = {}
+for team in teams:
+    bonus_stats[team] = {
+        'GP': 0
+    }
+    for cat in cats:
+        bonus_stats[team][cat] = [0, 0]
 
 # reads all of the spreadsheets in the given folder and subfolders and reads the data in each of the files
 for (dirpath, dirnames, filenames) in os.walk(directory):
     for filename in filenames:
         filepath = dirpath + '/' + filename
         print(filepath)
-        if filepath[-9:] == '.DS_Store':
+
+        # skip files that are do not have an excel file extension
+        if filepath[-5:] != '.xlsx':
             continue
 
         for game in pd.read_excel(filepath, sheet_name=None).values():
             # converts the pandas dataframe to a np.array
             game = np.append(np.array([game.columns]), game.to_numpy(), axis=0)
-            teams_in_game = []
+
+            teams_in_game = []  # array that contains the list of teams in the game
             question_row = get_question_row(game)
 
             for j in 1 + np.array(range(game.shape[1] - 1)):
                 player = str(game[question_row - 1, j]).title().strip()
+
+                # skip any player name that isn't valid
                 skip_player = player in ['', 'Nan']
                 for s in json_data['player names to ignore']:
                     skip_player = skip_player or (s in player)
-
                 if skip_player:
                     continue
-                
+
+                # regex that removes brackets [] from a player's name
                 player = re.sub('\[[^\]]*\]', '', player).strip()
 
                 # create a new player if the player isn't already in the database
@@ -117,7 +120,7 @@ for (dirpath, dirnames, filenames) in os.walk(directory):
                     if team not in teams_in_game:
                         teams_in_game.append(team)
 
-                player_stats[player]['GP'] += 1
+                player_stats[player]['GP'] += 1  # updated games played
 
                 # for each player, look down their respective column
                 # to collect data on when they buzzed
@@ -125,25 +128,22 @@ for (dirpath, dirnames, filenames) in os.walk(directory):
                     # cell that records their buzz
                     cell = str(game[i, j]).upper().strip()
 
+                    # check if we have reached a tossups heard cell
                     for string in [game[i, 0], game[i - 1, j]]:
                         if str(string).upper().strip() in ['PLAYER TUH TU HEARD'] and cell != 'NAN':
                             player_stats[player]['TUH'] += int(cell)
-                    
-                    # if str(game[i, 0]).upper().strip()  or str(game[i - 1, j]).upper().strip() in ['PLAYER TUH TU HEARD']:
-                    #     if cell != 'NAN':
-                    #         player_stats[player]['TUH'] += int(cell)        
 
-                    # category the question was in
-                    cat = 0
-                    if json_data['category order filepath'] == '':
-                        cat = get_category(str(game[i, 1]))
+                    # get the category the question was in
+                    if json_data['category directory'] == '':
+                        cat = game[i, 1]
                     else:
-                        if i - question_row >= 23:
-                            continue
-                        cat = get_category(open(json_data['category order filepath'], 'r').readlines()[
-                            i - question_row])
-                    
+                        cat = open(json_data['category directory'], 'r').readlines()[
+                            (i - question_row) % 23]
+                    cat = get_category(cat)
+
+                    # get what type of buzz was recorded (e.g. correct, interrupt)
                     index = get_code_index(cell)
+                    # add the buzz to the correct category
                     if index != -1:
                         if cat != 'n/a':
                             player_stats[player]['all'][index] += 1
@@ -155,46 +155,53 @@ for (dirpath, dirnames, filenames) in os.walk(directory):
             if len(teams_in_game) < 2:
                 continue
 
+            # find bonus stats
             n = 0
             for j in range(game.shape[1]):
                 if 'bonus' in str(game[1, j]).lower() or 'bonus' in str(game[0, j]).lower():
-                    for i in 2 + np.array(range(game.shape[0] - 2)):
+                    for i in range(game.shape[0]):
+                        # if the team got the bonus right
                         if str(game[i, j]).strip() in ['1', '1.0', '10', '10.0']:
-                            cat = get_category(str(game[i, 1]))
+                            if json_data['category directory'] == '':
+                                cat = game[i, 1]
+                            else:
+                                cat = open(json_data['category directory'], 'r').readlines()[
+                                    (i - question_row) % 23]
+                            cat = get_category(cat)
                             if cat == 'n/a':
                                 continue
-                            team_bonus_stats[teams_in_game[n]]['all'][0] += 1
-                            team_bonus_stats[teams_in_game[n]][cat][0] += 1
+                            bonus_stats[teams_in_game[n]]['all'][0] += 1
+                            bonus_stats[teams_in_game[n]][cat][0] += 1
                     n += 1
-            team_bonus_stats[teams_in_game[0]]['GP'] += 1
-            team_bonus_stats[teams_in_game[1]]['GP'] += 1
+            bonus_stats[teams_in_game[0]]['GP'] += 1
+            bonus_stats[teams_in_game[1]]['GP'] += 1
 
 
 cat_stats = {}      # dictionary containing per-category stats
 team_tu_stats = {}  # dictionary containing per-team tossup stats
 
 # initialize columns of the spreadsheet--indicates type of data included
+header = [
+    'Player',   # name of player
+    'GP',       # games played
+    '4I',       # interrupt correct
+    '4',        # correct (but no interrupt)
+    '-4',       # interrupt incorrect
+    'X',        # not interrupt, wrong buzz
+    'TUH',      # tossups heard
+    '#buzz',    # number of total buzzes
+    '%buzz',    # percent of tossups heard that the player buzzed
+    '%I',       # percent of tossups heard that the player interrupted
+    '4I/-4',    # interrupt corrects per neg
+    '4s/-4',    # corrects per neg
+    'P/TUH',    # points per tossup heard
+    'Points',   # total points
+    'PPG'       # points per game
+]
 for cat in cats:
-    temp = [
-        'Player',   # name of player
-        'GP',       # games played
-        '4I',       # interrupt correct
-        '4',        # correct (but no interrupt)
-        '-4',       # interrupt incorrect
-        'X',        # not interrupt, wrong buzz
-        'TUH',      # tossups heard
-        '#buzz',    # number of total buzzes
-        '%buzz',    # percent of tossups heard that the player buzzed
-        '%I',       # percent of tossups heard that the player interrupted
-        '4I/-4',    # interrupt corrects per neg
-        '4s/-4',    # corrects per neg
-        'P/TUH',    # points per tossup heard
-        'Points',   # total points
-        'PPG'       # points per game
-    ]
-    # initialize these arrays to deep copies of temp
-    cat_stats[cat] = [[i for i in temp]]
-    team_tu_stats[cat] = [[i for i in temp]]
+    # initialize these arrays to deep copies of the header
+    cat_stats[cat] = [[i for i in header]]
+    team_tu_stats[cat] = [[i for i in header]]
     team_tu_stats[cat][0][0] = 'Team'  # Change 'Player' to 'Team'
 
 
@@ -210,10 +217,7 @@ for team in teams:
 
 
 def player_to_team_num(player):
-    if player in rosters:
-        return team_to_number[rosters[player]]
-    else:
-        return -1
+    return -1 if player not in rosters else team_to_number[rosters[player]]
 
 
 # compiles per-category stats from the per-player stats
@@ -238,26 +242,8 @@ for player in player_stats:
 
         # TUH = tossups heard
         # this dictionary gives the number of tossups in each category per game
-        if json_data['track TUH']:
-            TUH = round(GP * {
-                'all': 23,
-                'bio': 4,
-                'chem': 4,
-                'energy': 3,
-                'ess': 4,
-                'math': 4,
-                'physics': 4
-            }[cat])
-        else:
-            TUH = round(TUH_total/23 * {
-                'all': 23,
-                'bio': 4,
-                'chem': 4,
-                'energy': 3,
-                'ess': 4,
-                'math': 4,
-                'physics': 4
-            }[cat])
+        TUH = round(json_data['cat per packet'][cat] *
+                    (GP if json_data['track TUH'] else TUH_total/23))
 
         # number of times the player buzzed
         num_buzz = fourI + four + neg + x1
@@ -266,10 +252,8 @@ for player in player_stats:
         pct_buzz = str(round(100*num_buzz/TUH, 2)) + '%'
 
         # percentage of buzzes that are an interrupt
-        if num_buzz != 0:
-            pct_I = str(round(100*(fourI + neg)/num_buzz, 2)) + '%'
-        else:
-            pct_I = 'N/A'
+        pct_I = str(round(100*(fourI + neg)/num_buzz, 2)) + \
+            '%' if num_buzz != 0 else 'N/A'
 
         if neg == 0:
             fourI_neg = 0 if fourI == 0 else 'inf'
@@ -305,20 +289,13 @@ for player in player_stats:
 
         team_data = team_tu_stats[cat][player_to_team_num(player)]  # team
         team_data[1] = max(
-            [team_data[1], GP, team_bonus_stats[rosters[player]]['GP']])  # GP
+            [team_data[1], GP, bonus_stats[rosters[player]]['GP']])  # GP
         team_data[2] += fourI   # fourI
         team_data[3] += four    # four
         team_data[4] += neg     # neg
         team_data[5] += x1      # X1
-        team_data[6] = max([team_data[6], TUH, round(team_data[1] * {
-            'all': 23,
-            'bio': 4,
-            'chem': 4,
-            'energy': 3,
-            'ess': 4,
-            'math': 4,
-            'physics': 4
-        }[cat])])  # TUH
+        team_data[6] = max([team_data[6], TUH, round(
+            team_data[1] * json_data['cat per packet'][cat])])  # TUH
         team_data[7] += num_buzz  # number of buzzes
         team_data[8] = str(
             round(100*team_data[7]/team_data[6], 2)) + '%'  # pct_buzz
@@ -330,8 +307,15 @@ for player in player_stats:
         team_data[12] = round(team_data[13]/team_data[6], 2)  # P_TUH
         team_data[14] = round(team_data[13]/team_data[1], 2)  # ppg
 
+
+# count how many tossups each team heard
+for cat in cats:
+    for i in range(len(teams)):
+        bonus_stats[teams[i]][cat][1] += int(
+            team_tu_stats[cat][i+1][2]) + int(team_tu_stats[cat][i+1][3])
+
 # compiles subject and bonus stats
-aggregate_subject = [[
+header = [  # spreadsheet header
     'Player',
     'GP',
     'ppg',
@@ -341,19 +325,10 @@ aggregate_subject = [[
     'ess',
     'math',
     'physics'
-]]
-
-aggregate_subject_team = [[
-    'Team',
-    'GP',
-    'ppg',
-    'bio',
-    'chem',
-    'energy',
-    'ess',
-    'math',
-    'physics'
-]]
+]
+aggregate_subject = [[i for i in header]]
+aggregate_subject_team = [[i for i in header]]
+aggregate_subject_team[0][0] = 'Team'
 
 for i in range(len(teams)):
     aggregate_subject_team.append([0]*9)
@@ -378,7 +353,7 @@ for i in range(len(teams)):
         array2[j+2] = team_tu_stats[cats[j]][i][-1]
 
 
-# converts team_bonus_stats, a dictionary, to team_bonus_stats_array,
+# converts bonus_stats, a dictionary, to bonus_stats_array,
 # a 2D array used for saving to an excel spreadsheet
 team_bonus_stats_array = [['team', 'GP']]
 for cat in cats:
@@ -386,11 +361,13 @@ for cat in cats:
     team_bonus_stats_array[0].append('')
     team_bonus_stats_array[0].append('%')
 
-for team in team_bonus_stats:
-    team_data = [team, team_bonus_stats[team]['GP']]
+for team in bonus_stats:
+    team_data = [team, bonus_stats[team]['GP']]
     for cat in cats:
-        correct = team_bonus_stats[team][cat][0]
-        total = team_bonus_stats[team][cat][1]
+        correct = bonus_stats[team][cat][0]
+        total = bonus_stats[team][cat][1]
+
+        # calculate the bonus conversion rate
         percentage = 0 if total == 0 else round(100*correct/total, 2)
         team_data.append(correct)
         team_data.append(total)
